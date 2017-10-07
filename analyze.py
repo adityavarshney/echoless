0db34ae44c6f507de6698f8a56855471266341df
Aditya Varshney <avarshney24@berkeley.edu>
2017-10-07 04:34:46 -0700

    watson related comments

--- analyze.py
+++ analyze.py
@@ -1,5 +1,5 @@
 import json
-from watson_developer_cloud import NaturalLanguageUnderstandingV1
+from watson_developer_cloud import NaturalLanguageUnderstandingV1, NaturalLanguageClassifierV1
 import watson_developer_cloud.natural_language_understanding.features.v1 \
   as Features
 
@@ -11,7 +11,28 @@ natural_language_understanding = NaturalLanguageUnderstandingV1(
   password=password,
   version="2017-02-27")
 
+natural_language_classifier = NaturalLanguageClassifierV1(
+  username=username,
+  password=password)
+
+# with open('../resources/weather_data_train.csv', 'rb') as training_data:
+#   classifier = natural_language_classifier.create(
+#     training_data=training_data,
+#     name='Politics Training',
+#     language='en'
+#   )
+
 def analyze_tweet(tweet):
+
+	# if len(tweet) < 70:
+	# 	classify_language(tweet)
+	# else:
+	# 	understand_language(tweet)
+	understand_language(tweet)
+
+
+def understand_language(tweet):
+	print(tweet)
 	response = natural_language_understanding.analyze(
 	  text=tweet,
 	  features=[
@@ -25,8 +46,19 @@ def analyze_tweet(tweet):
 	    Features.Keywords(
 	      sentiment=True,
 	      limit=3
-	    )
+	    ),
+	    Features.Keywords(
+	    	emotion=True,
+	    	sentiment=True,
+	    	limit=3
+	    ),
+	    Features.Categories()
 	  ]
 	)
 
 	print(json.dumps(response, indent=2))
+
+def classify_language(tweet):
+	return
+
+analyze_tweet('communism is awesome! i hate capitalism and the US is stupid')
