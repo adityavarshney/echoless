9e81d85a01f207cdfb0701aa97498a0927aff9a7
Gillian Chu <gillianchu@airbears2-10-142-133-33.airbears2.1918.berkeley.edu>
2017-10-07 14:47:51 -0700

    db stuff is working, just need to establish the right db

--- insertdb.py
+++ insertdb.py
@@ -1,16 +1,18 @@
-from pymongo import MongoClient, Connection
-import json
+from pymongo import MongoClient
+from tweets import process_friends_tweets
+import json, bson
+# from pymongo import DBObject 
+# from pymongo import JSON 
 
-client = MongoClient()
+client = MongoClient('mongodb://foo:bar@ds013495.mlab.com:13495/echoless')
 db = client['echoless']
 postings = db.postings_collection
 
-result = db.tweets.insert_many(
-	"hashtags": {
-	
-	}
-)
 
+db["_test2"].insert_many(process_friends_tweets())
+
+#result = db.tweets.insert_many(process_friends_tweets())
+# print(db["_test"].find_one())
 # hashtags
 # id
 # retweet_count
